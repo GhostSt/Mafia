3649de182a26c73fc38c290ce964f7fac33726c0
Victor <victor.diditskiy@yandex.ru>
2017-03-09 10:47:08 +0300

    add UserAdmin

---
+++ src/GhostSt/CoreBundle/Admin/UserAdmin.php
@@ -0,0 +1,46 @@
+<?php
+
+// src/GhostSt/CoreBundle/Admin/UserAdmin.php
+
+namespace GhostSt\CoreBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class UserAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form_mapper)
+    {
+        $form_mapper
+            ->add('username', null, [
+                'label' => 'admin.user.form.username'
+            ]);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagrid_mapper)
+    {
+    }
+
+    protected function configureListFields(ListMapper $list_mapper)
+    {
+        $list_mapper
+            ->add('username', null, [
+                'label' => 'admin.user.list.username',
+            ])
+            ->add('_action', 'actions', [
+                'label'   => 'admin.user.list.actions.edit',
+                'actions' => [
+                    'edit' => [
+                        'template' => 'SonataAdminBundle:CRUD:list__action_edit.html.twig',
+                    ],
+                ],
+            ]);
+    }
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+    }
+}
