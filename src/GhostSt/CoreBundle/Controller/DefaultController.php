dc7b838598c35314af3f0ed3632d836efa746f54
Victor <victor.diditskiy@yandex.ru>
2017-04-02 21:07:29 +0300

    set up redirect to login form

--- src/GhostSt/CoreBundle/Controller/DefaultController.php
+++ src/GhostSt/CoreBundle/Controller/DefaultController.php
@@ -11,27 +11,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $dm = $this->get('doctrine_mongodb')->getManager();
-        /*
-        $user = new User();
-        $user->setUsername('GhostSt');
-        $user->setEmail('ghostst@mail.ru');
-        $user->setPassword('Qwerty');
-        $dm->persist($user);
-        $dm->flush();
-        */
-        $users = $dm->getRepository('GhostStCoreBundle:User')->findAll();
-        var_dump($users);
-
-        $userRoles = $dm->getRepository('GhostStCoreBundle:UserRole')->findAll();
-        var_dump($userRoles);
-
-        $games = $dm->getRepository('GhostStCoreBundle:Game')->findAll();
-        /** @var Game $game */
-        $game = $games[0];
-
-        var_dump($game);
-
-        return $this->render('GhostStCoreBundle:Default:index.html.twig');
+        return $this->redirectToRoute('_login');
     }
 }
